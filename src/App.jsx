e87ad4051e8b9cf3b404444f275365cd555d9eb1
Jagmohan Singh <jattiphrswan49@gmail.com>
2024-10-22 17:43:49 +0530

    again changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ function App() {
   const [numberAllowed, setNumberAllowed] = useState(false)
   const [charAllowed, setCharAllowed] = useState(false)
   const [password, setPassword] = useState('')
-
  
 
   const generatePassword = useCallback(()  => {
